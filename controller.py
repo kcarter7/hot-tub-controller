b8a2d2c0f3ae327a4e4a53426f59a4a64a1ec35b
Dan Hardy <hardydb@gmail.com>
2016-06-06 03:29:58 +0000

    Reworked pins for easier placement, use heater off/on with SSR instead of older circuit (exerpimental)

--- controller.py
+++ controller.py
@@ -4,13 +4,15 @@ import RPi.GPIO as GPIO
 class Controller(object):
 
     PUMP1_LOW = 11
-    PUMP1_HIGH = 12
-    PUMP2 = 13
-    HEATER = 7
+    PUMP1_HIGH = 13
+    PUMP2 = 15
+    HEATER = 29
+    NOT_HEATER = 31
 
     def __init__(self):
         GPIO.setup([self.PUMP1_LOW, self.PUMP1_HIGH, self.PUMP2, self.HEATER],
                    GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup([self.NOT_HEATER], GPIO.OUT, initial=GPIO.HIGH)
 
     def pump1_off(self):
         GPIO.output([self.PUMP1_LOW, self.PUMP1_HIGH], GPIO.LOW)
@@ -28,7 +30,7 @@ class Controller(object):
         GPIO.output(self.PUMP2, GPIO.HIGH)
 
     def heater_off(self):
-        GPIO.output(self.HEATER, GPIO.LOW)
+        GPIO.output([self.HEATER, self.NOT_HEATER], (GPIO.LOW, GPIO.HIGH))
 
     def heater_on(self):
-        GPIO.output(self.HEATER, GPIO.HIGH)
+        GPIO.output([self.HEATER, self.NOT_HEATER], (GPIO.HIGH, GPIO.LOW))
